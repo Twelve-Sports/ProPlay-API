332b2eebd55b9f3eb608a63d98d9e261be1ddebd
Pedro Lobato <Pedrosslobato@gmail.com>
2023-11-13 09:59:24 -0300

    Configurando rotas

--- routes.js
+++ routes.js
@@ -1,5 +1,14 @@
-import { Router } from "express";
+import { Router, json } from "express";
+import { createRec, indexRec } from "./controllers/recordController.js";
+import { createClipe, indexClipe } from "./controllers/clipsController.js";
 const router = Router();
+router.use(json())
 
+
+router.get("/rec", indexRec)
+      .post("/rec", createRec)
+
+router.get("/clipe", indexClipe)
+      .post("/clipe", createClipe)
 
 export default router;
