8916fc1a7ebe38dd4828307f22a0fa16c6ee4dd3
Pedro Lobato <Pedrosslobato@gmail.com>
2023-11-22 09:48:01 -0300

    tirando o .env do banco

--- database/knexfile.js
+++ database/knexfile.js
@@ -5,11 +5,18 @@ dotenv.config();
 export const development = {
   client: 'mysql2',
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'lobatoSQL',
+    password: '1234',
+    database: 'gravacoes',
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './migrations',
+  },
+  seeds: {
+    directory: './seeds',
   },
 };
 
