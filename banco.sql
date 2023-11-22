931542244e7714d8a51eae36dc3d76b29c47a2f8
Pedro Lobato <Pedrosslobato@gmail.com>
2023-11-22 09:47:17 -0300

    Arrumando o banco

--- banco.sql
+++ banco.sql
@@ -2,18 +2,28 @@ DROP DATABASE gravacoes;
 CREATE DATABASE gravacoes;
 USE gravacoes;
 
-CREATE TABLE record (
-  id int PRIMARY KEY AUTO_INCREMENT,
-  file varchar(255),
-  data datetime,
-  court varchar(50)
+CREATE TABLE court (
+  id INT PRIMARY KEY,
+  name VARCHAR(255)
 );
 
-CREATE TABLE clips(
-  id int PRIMARY KEY AUTO_INCREMENT,
-  file varchar(255),
-  timestamp datetime,
+CREATE TABLE record (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  file VARCHAR(255),
+  data DATETIME
+);
+
+CREATE TABLE clips (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  file VARCHAR(255),
+  timestamp DATETIME,
   record_id INT NOT NULL,
   FOREIGN KEY (record_id) REFERENCES record(id)
 );
 
+CREATE TABLE dateNow (
+  id INT PRIMARY KEY AUTO_INCREMENT,
+  data VARCHAR(255),
+  court_id INT NOT NULL,
+  FOREIGN KEY (court_id) REFERENCES court(id)
+);
