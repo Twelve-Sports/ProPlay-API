271b9d05a378025b9cdb7e16862f44e2f690db10
Pedro Lobato <Pedrosslobato@gmail.com>
2023-11-27 10:03:33 -0300

    Arrumando banco

--- banco.sql
+++ banco.sql
@@ -1,29 +1,31 @@
-DROP DATABASE gravacoes;
-CREATE DATABASE gravacoes;
-USE gravacoes;
+  DROP DATABASE gravacoes;
+  CREATE DATABASE gravacoes;
+  USE gravacoes;
 
-CREATE TABLE court (
-  id INT PRIMARY KEY,
-  name VARCHAR(255)
-);
+  CREATE TABLE court (
+    id INT PRIMARY KEY,
+    name VARCHAR(255)
+  );
 
-CREATE TABLE record (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  file VARCHAR(255),
-  data DATETIME
-);
+  CREATE TABLE record (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    file VARCHAR(255),
+    data DATETIME
+  );
 
-CREATE TABLE clips (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  file VARCHAR(255),
-  timestamp DATETIME,
-  record_id INT NOT NULL,
-  FOREIGN KEY (record_id) REFERENCES record(id)
-);
+  CREATE TABLE clips (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    file VARCHAR(255),
+    timestamp DATETIME,
+    record_id INT NOT NULL,
+    FOREIGN KEY (record_id) REFERENCES record(id)
+  );
 
-CREATE TABLE dateNow (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  data VARCHAR(255),
-  court_id INT NOT NULL,
-  FOREIGN KEY (court_id) REFERENCES court(id)
-);
+  CREATE TABLE dateNow (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    data VARCHAR(255),
+    court_id INT NOT NULL,
+    isDone BOOLEAN DEFAULT false,
+    FOREIGN KEY (court_id) REFERENCES court(id)
+  );
+
